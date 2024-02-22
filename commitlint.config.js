0c295eb5472026944de36636e139d5643de1b0c5
Jason Williams <jasonwilliams2308@gmail.com>
2024-02-22 00:30:39 +0000

    feat(develop): test

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,1 @@
-module.exports = {
-    extends: ["@commitlint/config-conventional"],
-    rules: {
-      "scope-max-length": [2, "always", 50],
-    },
-  };
+module.exports = {extends: ['@commitlint/config-conventional']};
